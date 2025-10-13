4b03d6695550fbc4122ad512bca6cd5b63b9045e
Faltukaam <faltu1717kaam@gmail.com>
2025-10-13 12:39:49 +0000

    letter D should be red and heart color should be off red and circle colo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -13,20 +13,20 @@ export function DVLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      {/* Outer circle - Blue */}
-      <circle cx="12" cy="12" r="11" stroke="blue" />
+      {/* Outer circle - Off-white */}
+      <circle cx="12" cy="12" r="11" stroke="#F5F5F5" />
       
-      {/* Heart shape - Red */}
-      <path d="M12 7.5a3 3 0 0 1 3-3c1.657 0 3 1.343 3 3 0 1.83-2.053 3.832-6 6.832-3.947-3-6-4.998-6-6.832 0-1.657 1.343-3 3-3a3 3 0 0 1 3 3z" stroke="red" fill="red" />
+      {/* Heart shape - Off-Red */}
+      <path d="M12 7.5a3 3 0 0 1 3-3c1.657 0 3 1.343 3 3 0 1.83-2.053 3.832-6 6.832-3.947-3-6-4.998-6-6.832 0-1.657 1.343-3 3-3a3 3 0 0 1 3 3z" stroke="#CD5C5C" fill="#CD5C5C" />
 
-      {/* Text "D" - Green */}
+      {/* Text "D" - Red */}
       <text 
         x="12" 
         y="19" 
         textAnchor="middle" 
         fontSize="8" 
         fontWeight="bold" 
-        fill="green"
+        fill="red"
         stroke="none"
       >
         D
